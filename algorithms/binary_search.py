3e8f9d2475e86be276caf0fee9ae5ae9aac354a0
symonk <jackofspaces@gmail.com>
2023-11-04 12:44:46 +0000

    implementing `binary_search`

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,4 +1,18 @@
 def binary_search(haystack: list[int], needle: int) -> bool:
-    """Detect if an (ordered) sequence contains a particular
-    value in O(log n) time."""
+    """Iterative binary search.  Detects if a value is in a
+    sorted sequence in O(log n) time, avoiding the overhead of
+    a linear search.
+
+    :param haystack: The ordered sequence.
+    :param needle: The target to find.
+    """
+    low, high = 0, len(haystack) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if haystack[mid] == needle:
+            return True
+        if haystack[mid] < needle:
+            low = mid + 1
+        else:
+            high = mid - 1
     return False
--- algorithms/binary_search_test.py
+++ algorithms/binary_search_test.py
@@ -1,2 +1,9 @@
-def test_binary_search():
-    ...
+from binary_search import binary_search
+
+
+def test_binary_search_in():
+    assert binary_search(list(range(10_000)), 500)
+
+
+def test_binary_search_not_in():
+    assert not binary_search([3, 4, 5, 6, 7], 1)
