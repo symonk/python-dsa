d74d8b5853fc586be32986982b366e8110b3dc3c
symonk <jackofspaces@gmail.com>
2023-11-04 12:02:06 +0000

    adding `docs`; configure basic `tests`

--- algorithms/binary_search.py
+++ algorithms/binary_search.py
@@ -1,7 +1,4 @@
-import typing
-
-
-def binary_search(haystack: typing.List[int], needle: int) -> bool:
+def binary_search(haystack: list[int], needle: int) -> bool:
     """Detect if an (ordered) sequence contains a particular
     value in O(log n) time."""
     return False
--- algorithms/binary_search_test.py
+++ algorithms/binary_search_test.py
@@ -1,3 +1,2 @@
-
 def test_binary_search():
     ...
