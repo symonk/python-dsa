cf6f8c570a652f66afa04a22d1dba4e7d3acbada
symonk <jackofspaces@gmail.com>
2023-11-04 17:26:25 +0000

    `symmetric_difference` in sets.

--- data_structures/sets.py
+++ data_structures/sets.py
@@ -48,6 +48,49 @@ def clear():
 
 
 @big_o(time_complexity=Complexity.LINEAR)
+def difference():
+    """Return the difference of one more sets.  Difference
+    refers to elements in the initial set, minus those in the others.
+    Elements in set A that do not exist in B, ..., n.
+
+    The difference method accepts a single set as an alternative parameter,
+    however it implements the `-` operator to enable passing of multiple
+    iterables.
+
+    >>> x = set((1,2,3))
+    >>> y = set((2,3,4))
+    >>> x.difference(y)
+    {1}
+    >>> x - y
+    {1}
+    >>> x - y - set((1,))
+    set()
+    """
+
+
+@big_o(time_complexity=Complexity.LINEAR)
+def symmetric_difference():
+    """Unlike `set.difference`.  the symmetric difference
+    of one more sets is the elements which exist in ANY
+    of the sets, but that do not exist in multiple.  Items
+    which do not overlap at all.
+
+    Set symmetric difference is implemented via the `^` operator
+    and as normal, allows multiple iterables to be provided.
+
+    >>> x = set([99, 88, 77])
+    >>> y = set([66, 55, 44])
+    >>> z = set([11, 22, 33])
+    >>> x ^ y ^ z == {11, 22, 33, 44, 55, 66, 77, 88, 99}
+    True
+    >>> x.symmetric_difference(z) == {11, 22, 33, 99, 88, 77}
+    True
+    >>> x ^ set((99, 88, 77))
+    set()
+    """
+
+
+@big_o(time_complexity=Complexity.LINEAR)
 def copy():
     """Creates a shallow copy of the set.
     >>> x = set((1,2,3))
