b3412efdf7ddbc5bd827f51267489356f7fbdff7
symonk <jackofspaces@gmail.com>
2023-11-04 13:07:18 +0000

    adding basics for `sets`

---
+++ data_structures/sets.py
@@ -0,0 +1,16 @@
+"""
+This outlined everything to do with sets, specifically in python however a lot of the content
+and venn diagrams etc apply to general set theory and provide a generic understanding.
+
+Python sets:
+    * Unordered, non indexable collections of distinct, hashable elements.
+    * Python offers both the `set` (unhashable) and the `frozenset` (hashable)
+"""
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
