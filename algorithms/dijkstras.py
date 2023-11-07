376c75f5035b03705dfbc88b1acd712def0c329d
Simon K <jackofspaces@gmail.com>
2023-11-07 13:39:42 +0000

    Create dijkstras.py

---
+++ algorithms/dijkstras.py
@@ -0,0 +1,20 @@
+"""
+Dijkstras Algorithm is search algorithma for detecting the shortest
+path between two nodes.
+
+This algorithm is only suitable for problems that can be represented
+in a graph-like structure.
+
+To Better understand this algorithm, consider the following scenario:
+
+Person [A] is attempting to cycle from Reykjavik to Belgrade, however
+there are various different ways to get there.
+
+1: Reykjavik –> London –> Berlin –> Rome –> Athens –> Moscow –> Belgrade
+2: Reykjavik -> Oslo -> -> Beerlin -> Belgrade
+... (and so on and so fourth).
+"""
+
+def dijkstras() -> None:
+  """The algorithm implementation."""
+  ...
