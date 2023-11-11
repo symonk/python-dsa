05176e0b07d139d378b31ee28d6a35d1866d16a0
symonk <jackofspaces@gmail.com>
2023-11-11 11:20:38 +0000

    add improved solution for `49: Group Anagrams` using `ord()`

--- problems/49_group_anagrams.py
+++ problems/49_group_anagrams.py
@@ -7,3 +7,16 @@ class Solution:
         for string in strs:
             store["".join(sorted(string))].append(string)
         return list(store.values())
+
+
+class ImprovedSolution:
+    # Note: We know the input is only lowercase english, so we can make the counting
+    # checks O(n) across the strings rather than sorting them
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        store = defaultdict(list)
+        for s in strs:
+            seq = [0] * 26  # lowercase english
+            for char in s:
+                seq[ord(char) - ord("a")] += 1
+            store[tuple(seq)].append(s)
+        return list(store.values())
