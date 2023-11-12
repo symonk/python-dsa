e422ee7a5316749f8137a36c9f313f47ba001a30
symonk <jackofspaces@gmail.com>
2023-11-12 11:10:31 +0000

    improved `74: Binary Search 2D Matrix`

--- problems/binary_search/74_search_a_2d_matrix.py
+++ problems/binary_search/74_search_a_2d_matrix.py
@@ -4,7 +4,19 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
             val = matrix[mid]
-            if target in val:
+            row_res = False
+            row_l, row_r = 0, len(val) - 1
+            while row_l <= row_r:
+                row_mid = (row_l + row_r) // 2
+                row_val = val[row_mid]
+                if row_val == target:
+                    row_res = True
+                    break
+                if row_val > target:
+                    row_r = row_mid - 1
+                else:
+                    row_l = row_mid + 1
+            if row_res is True:
                 return True
             if val[0] > target:
                 high = mid - 1
