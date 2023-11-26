27043deea80bfb220978750473d7e7129e271cd2
Simon K <jackofspaces@gmail.com>
2023-11-26 10:03:01 +0000

    tweaks to `default_dict.py`

--- data_structures/default_dict.py
+++ data_structures/default_dict.py
@@ -3,14 +3,22 @@ an implementation of dunder __missing__ for handling
 missing keys.  A factory function provided is calling
 for missing values. defaultdict exhibits all the same
 performance of its super class `dict`. """
-from collections import defaultdict
 
 
-def test_default_dict() -> None:
-    def always_100() -> int:
-        return 100
+def default_dict() -> None:
+    """
+    >>> from collections import defaultdict
+    >>> def always_100(): return 100
+    >>> d = defaultdict(always_100)
+    >>> d[1]
+    100
+    >>> d[2]
+    200
+    >>> d[2] += 1000
+    >>> d[2]
+    1100
+    """
 
-    d: dict[int, int] = defaultdict(always_100)
-    d[1]
-    d[2]
-    assert d == {1: 100, 2: 100}
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
