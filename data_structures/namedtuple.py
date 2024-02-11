0adeae893d890c4fc637eb4722c27103ac6f2ee1
Simon K <jackofspaces@gmail.com>
2024-02-11 11:09:42 +0000

    additional docs on `namedtuple`

---
+++ data_structures/namedtuple.py
@@ -0,0 +1,29 @@
+"""Named tuple is a fancier tuple that offers attribute based access
+for records or tuple like types.  There are two ways to define a named
+tuple, the typing variant is preferred if you want to offer proper type
+hinting.  Named tuples are immutable like their tuple equivalents and
+that is the main reason why a named tuple may be preferred to using a
+dataclass, however with froze=True that is less important.
+
+Use namedtuples when you want an immutable sequence and as such they are
+hashable.
+"""
+from collections import namedtuple
+from typing import NamedTuple
+
+def main() -> int:
+    record = namedtuple("Record", "name age score")
+
+    class Record2(NamedTuple):
+        """Largely equivalent to record"""
+        name: str
+        age: int
+        score: int
+
+    t1 = record("john", 18, 100)
+    t2 = Record2("doe", 30, 75)
+
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
